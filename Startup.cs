54574432a2bed66cfa0b886a37cd07ee2b08ce91
Zepp <zeppsoftapp@gmail.com>
2024-08-01 16:05:56 +0300

    Init

---
+++ Startup.cs
@@ -0,0 +1,29 @@
+namespace DutchTreat;
+
+public class Startup
+{
+    public void ConfigureServices(IServiceCollection services)
+    {
+        
+    }
+
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+        if (env.IsDevelopment())
+        {
+            app.UseDeveloperExceptionPage();
+        }
+
+        // app.UseRouting();
+        // app.UseEndpoints(endpoints =>
+        //     endpoints.MapGet("/", async context =>
+        //     {
+        //         await context.Response.WriteAsync("Hello, Zepp!");
+        //     }));
+        
+        app.Run(async context =>
+        {
+            await context.Response.WriteAsync("Hello, Zepp!");
+        });
+    }
+}
