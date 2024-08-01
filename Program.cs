09f27e6e430eafdcd9379e0b33bf4fe423773ae6
Zepp <zeppsoftapp@gmail.com>
2024-08-01 17:45:51 +0300

    Comments cleaned

--- Program.cs
+++ Program.cs
@@ -4,12 +4,6 @@ public class Program
 {
     public static void Main(string[] args)
     {
-        // var builder = WebApplication.CreateBuilder(args);
-        // var app = builder.Build();
-        //
-        // app.MapGet("/", () => "Hello World!");
-        //
-        // app.Run();
         CreateHostBuilder(args).Build().Run();
     }
 
